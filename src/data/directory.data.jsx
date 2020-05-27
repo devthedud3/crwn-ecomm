bcdf3808541228a086ab7dd71832dfe141fc8986
ccushnie <Corey.Cushnie@yahoo.com>
2020-05-27 17:38:00 -0400

    data fill removed from gitignore

---
+++ src/data/directory.data.jsx
@@ -0,0 +1,36 @@
+const DIRECTORY_DATA = [
+  {
+    id: 1,
+    name: 'hats',
+    imageUrl: 'https://tinyurl.com/ycu9zo7s',
+    link: 'shop/hats'
+  },
+  {
+    id: 2,
+    name: 'jackets',
+    imageUrl: 'https://tinyurl.com/y7v2rnjj',
+    link: 'shop/jackets'
+  },
+  {
+    id: 3,
+    name: 'sneakers',
+    imageUrl: 'https://tinyurl.com/ya7oosq9',
+    link: 'shop/sneakers'
+  },
+  {
+    id: 5,
+    name: 'womens',
+    size: 'large',
+    imageUrl: 'https://tinyurl.com/ybgbre5c',
+    link: 'shop/womens'
+  },
+  {
+    id: 4,
+    name: 'mens',
+    size: 'large',
+    imageUrl: 'https://tinyurl.com/y9nejpjj',
+    link: 'shop/mens'
+  }
+];
+
+export default DIRECTORY_DATA;
